611f1e3897953df3dc05e43bc9ac28e960cd5515
ComradeVanti <kingvanti650@gmail.com>
2022-03-31 18:16:43 +0200

    Add app

--- AdapterArray1/AdapterArray1.App/Program.cs
+++ AdapterArray1/AdapterArray1.App/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AdapterArray1;
+
+var path = args[0];
+
+var input = File.ReadLines(path).Select(int.Parse).ToArray();
+
+var output = AdapterArray.CalcJoltageRating(input);
+
+Console.WriteLine($"The output is: {output}");
