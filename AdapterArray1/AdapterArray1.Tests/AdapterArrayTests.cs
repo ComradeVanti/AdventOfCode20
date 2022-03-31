b70f098d6538e8af14be9427997025e45601b291
ComradeVanti <kingvanti650@gmail.com>
2022-03-31 17:22:51 +0200

    Drop incorrect property

--- AdapterArray1/AdapterArray1.Tests/AdapterArrayTests.cs
+++ AdapterArray1/AdapterArray1.Tests/AdapterArrayTests.cs
@@ -9,16 +9,5 @@ public class AdapterArrayTests
     [Property]
     public bool OutputIsPositive(Input input) =>
         AdapterArray.CalcJoltageRating(input.Joltages) > 0;
-
-    [Property]
-    public bool DiffBetweenInputsAndInputsPlusOneIsOne(Input original)
-    {
-        var plusOne = new Input(original.Joltages.Select(it => it + 1).ToArray());
-
-        var originalOutput = AdapterArray.CalcJoltageRating(original.Joltages);
-        var plusOneOutput = AdapterArray.CalcJoltageRating(plusOne.Joltages);
-
-        return originalOutput + 1 == plusOneOutput;
-    }
     
 }
