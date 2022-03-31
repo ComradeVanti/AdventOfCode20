e80fa564ae6f722c5f6c9de2aab83e633c5a253d
ComradeVanti <kingvanti650@gmail.com>
2022-03-31 17:17:31 +0200

    Add adapter array 1 test

--- AdapterArray1/AdapterArray1.Tests/AdapterArrayTests.cs
+++ AdapterArray1/AdapterArray1.Tests/AdapterArrayTests.cs
@@ -1,5 +1,5 @@
-﻿using FsCheck.Xunit;
-using Xunit;
+﻿using System.Linq;
+using FsCheck.Xunit;
 
 namespace AdapterArray1;
 
@@ -10,4 +10,15 @@ public class AdapterArrayTests
     public bool OutputIsPositive(Input input) =>
         AdapterArray.CalcJoltageRating(input.Joltages) > 0;
 
+    [Property]
+    public bool DiffBetweenInputsAndInputsPlusOneIsOne(Input original)
+    {
+        var plusOne = new Input(original.Joltages.Select(it => it + 1).ToArray());
+
+        var originalOutput = AdapterArray.CalcJoltageRating(original.Joltages);
+        var plusOneOutput = AdapterArray.CalcJoltageRating(plusOne.Joltages);
+
+        return originalOutput + 1 == plusOneOutput;
+    }
+    
 }
