7c26fcf43ed075ec2de300e3c1a743cd227ba474
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2022-03-31 18:04:53 +0200

    Implement calc

--- AdapterArray1/AdapterArray1.Core/AdapterArray.cs
+++ AdapterArray1/AdapterArray1.Core/AdapterArray.cs
@@ -3,7 +3,30 @@
 public static class AdapterArray {
 
 	public static int CalcJoltageRating(int[] joltages) {
-		return 1;
+
+		var sortedJoltages = joltages.OrderBy(it => it).ToArray();
+		var oneCount = 0;
+		var threeCount = 1;
+
+		var firstDiff = sortedJoltages[0];
+
+		if (firstDiff == 3)
+			threeCount++;
+
+		if (firstDiff == 1)
+			oneCount++;
+
+		for (var i = 0; i < sortedJoltages.Length-1; i++) {
+			var diff = sortedJoltages[i + 1] - sortedJoltages[i];
+
+			if (diff == 3)
+				threeCount++;
+
+			if (diff == 1)
+				oneCount++;
+		}
+		
+		return threeCount * oneCount;
 	}
 
 }
