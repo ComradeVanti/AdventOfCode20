d994b690f5570dcee804f6d3c12baf1de4966f11
ComradeVanti <kingvanti650@gmail.com>
2022-04-06 11:19:23 +0200

    Add app
    For report repair 2

--- ReportRepair2/ReportRepair2.App/Program.cs
+++ ReportRepair2/ReportRepair2.App/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ReportRepair2;
+
+var input = File.ReadAllLines(args[0])
+                .Select(int.Parse);
+
+var output = Report.CalcVerificationNum(input);
+
+Console.WriteLine($"Output is: {output}");
