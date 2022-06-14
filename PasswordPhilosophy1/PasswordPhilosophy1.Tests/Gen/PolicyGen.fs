ef7463f5ae741d439baa36ea310bb88bff3323ef
ComradeVanti <kingvanti650@gmail.com>
2022-06-14 13:16:59 +0200

    feat: logic to generate policies

---
+++ PasswordPhilosophy1/PasswordPhilosophy1.Tests/Gen/PolicyGen.fs
@@ -0,0 +1,23 @@
+﻿module PasswordPhilosophy1.PolicyGen
+
+open FsCheck
+
+let genLetter = Gen.choose (97, 122) |> Gen.map char
+
+let genMinCount =
+    Gen.choose (Policy.MinMinCount, Policy.MaxMinCount)
+
+let genMaxCount minCount =
+    Gen.choose (Policy.MinRange, Policy.MaxRange)
+    |> Gen.map (fun offset -> minCount + offset)
+
+let genPolicy =
+    gen {
+        let! letter = genLetter
+        let! minCount = genMinCount
+        let! maxCount = genMaxCount minCount
+        return Policy(letter, minCount, maxCount)
+    }
+
+type ArbPolicy =
+    static member Default() = Arb.fromGen genPolicy
