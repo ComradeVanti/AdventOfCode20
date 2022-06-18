80eb7ff46e04f3652977b8aa5c721c9983bc5462
ComradeVanti <kingvanti650@gmail.com>
2022-06-18 17:11:51 +0200

    feat: add program

--- PasswordPhilosophy1/PasswordPhilosophy1.App/Program.cs
+++ PasswordPhilosophy1/PasswordPhilosophy1.App/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using PasswordPhilosophy1;
+
+var path = args[0];
+var lines = File.ReadLines(path);
+var logs = lines.Select(PasswordLog.Parse);
+var validCount = Validation.CountValid(logs!);
+
+Console.WriteLine($"Valid: {validCount}");
