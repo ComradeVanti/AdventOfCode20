f2bdb47389a5e70ced1abf2563220e6af5ecb158
ComradeVanti <kingvanti650@gmail.com>
2022-06-25 19:49:38 +0200

    feat: collect options

---
+++ Common.Lib/OptionExt.fs
@@ -0,0 +1,7 @@
+﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
+module AdventOfCode20.Option
+
+let rec collect opts =
+    match opts with
+    | [] -> Some []
+    | head :: tail -> Option.map2 (fun h t -> h :: t) head (collect tail)
