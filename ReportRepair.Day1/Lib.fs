5e563f9b96451f53b2f91a8e88edc26ef5e90e44
ComradeVanti <kingvanti650@gmail.com>
2022-06-27 20:06:47 +0200

    refactor: report-repair day 1 for simplicity

--- ReportRepair.Day1/Lib.fs
+++ ReportRepair.Day1/Lib.fs
@@ -5,16 +5,9 @@ open AdventOfCode20
 open AdventOfCode20.ReportRepair
 
 let private find2020Pair (Entries expenses) =
-
-    let tryFindPairFrom index =
-        let expense = expenses |> List.item index
-
-        expenses
-        |> List.skip (index + 1)
-        |> List.tryFind (fun other -> other + expense = 2020)
-        |> Option.map (fun other -> (expense, other))
-
-    expenses |> List.indices |> List.tryPick tryFindPairFrom
+    expenses
+    |> List.pairs
+    |> Seq.tryFind (fun (a, b) -> a + b = 2020)
 
 
 let private mult (a, b) = a * b
