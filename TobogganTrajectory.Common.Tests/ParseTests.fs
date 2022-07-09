aefb9747788aece90d1501ea98c0e06fe90c0403
ComradeVanti <kingvanti650@gmail.com>
2022-07-09 18:56:10 +0200

    feat: parse input for toboggan-trajectory

---
+++ TobogganTrajectory.Common.Tests/ParseTests.fs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode20.TobogganTrajectory
+
+open AdventOfCode20.Props
+open AdventOfCode20.TobogganTrajectory.ForestMapGen
+open FsCheck.Xunit
+
+
+[<Properties(Arbitrary = [| typeof<ArbForestMaps> |])>]
+module ParseTests =
+
+    let private stringify map =
+
+        let stringifyTile tile =
+            match tile with
+            | Tree -> '#'
+            | Empty -> '.'
+
+        let stringifyRow row =
+            row |> List.map stringifyTile |> System.String.Concat
+
+        ForestMap.rowsOf map |> List.map stringifyRow
+
+    [<Property>]
+    let ``Maps are parsed correctly`` map =
+        let lines = stringify map
+
+        match Parse.forestMap lines with
+        | Some parsed -> parsed =? map
+        | None -> rejectWith "Could not parse"
---
+++ TobogganTrajectory.Common/Parse.fs
@@ -0,0 +1,20 @@
+﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
+module AdventOfCode20.TobogganTrajectory.Parse
+
+open System
+open AdventOfCode20
+open AdventOfCode20.TobogganTrajectory
+
+let private parseTile char =
+    if char = '.' then Some Empty
+    elif char = '#' then Some Tree
+    else None
+
+let private parseRow line =
+    line |> Seq.map parseTile |> Seq.toList |> Option.collect
+
+let forestMap (lines: string list) =
+    lines
+    |> List.map parseRow
+    |> Option.collect
+    |> Option.map Tiles
