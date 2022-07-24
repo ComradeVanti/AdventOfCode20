7db026ff6318621699ceaf57e5910dc82067b4f6
ComradeVanti <kingvanti650@gmail.com>
2022-07-24 20:38:32 +0200

    feat: include and load example-input

---
+++ _00Common.Test/ExampleInput.fs
@@ -0,0 +1,17 @@
+﻿[<Microsoft.FSharp.Core.RequireQualifiedAccess>]
+module AdventOfCode20.ExampleInput
+
+open System.IO
+open System.Reflection
+
+let private resourceNameIn (assembly: Assembly) =
+    assembly.GetManifestResourceNames()
+    |> Array.find (fun name -> name.EndsWith("example-input.txt"))
+
+let content assembly =
+    let resourceName = resourceNameIn assembly
+
+    using (assembly.GetManifestResourceStream(resourceName)) (fun stream ->
+        using (new StreamReader(stream)) (fun reader -> reader.ReadToEnd()))
+
+let lines assembly = content assembly |> String.splitAt '\n'
